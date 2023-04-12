3b2525ac7aea25b0646f5e0ae0ff5775729edc78
Deepak Kumar <cu.18bcs2295@gmail.com>
2023-04-12 12:17:52 +0530

    add structured faq pattern

---
+++ patterns/structured-faq.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Structured FAQ Section
+ *
+ * slug: structured-faq-section
+ * title: Structured FAQ Section
+ * categories: dev-talks
+ */
+
+return array(
+	'title'      => __( 'Structured FAQ Section', 'dev-talks' ),
+	'categories' => array( 'dev-talks' ),
+	'content'    => '<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20"},"margin":{"top":"0","bottom":"0"}},"color":{"background":"#b1d5f0"}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group has-background" style="background-color:#b1d5f0;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:heading {"textAlign":"left","level":3,"style":{"spacing":{"margin":{"right":"0","top":"var:preset|spacing|30"}}}} -->
+	<h3 class="wp-block-heading has-text-align-left" style="margin-top:var(--wp--preset--spacing--30);margin-right:0">Frequently Asked Questions In An Interview</h3>
+	<!-- /wp:heading -->
+
+	<!-- wp:separator {"className":"is-style-wide"} -->
+	<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+	<!-- /wp:separator -->
+
+	<!-- wp:dev-talks/structured-faq -->
+	<div itemtype="https://schema.org/FAQPage" itemscope><!-- wp:dev-talks/structured-question-group -->
+	<div itemtype="https://schema.org/Question" itemscope itemprop="mainEntity"><!-- wp:heading {"placeholder":"Question","hasItemProp":true} -->
+	<h2 class="wp-block-heading" itemprop="name">What type of development are you currently working on?</h2>
+	<!-- /wp:heading -->
+
+	<!-- wp:dev-talks/structured-answer -->
+	<div itemtype="https://schema.org/Answer" itemscope itemprop="acceptedAnswer"><div itemprop="text"><!-- wp:paragraph {"placeholder":"Answer"} -->
+	<p>When hiring managers ask this, they want to know that you understand your contribution to a company. When you answer, explain the business reasoning for your everyday work as a developer.</p>
+	<!-- /wp:paragraph --></div></div>
+	<!-- /wp:dev-talks/structured-answer --></div>
+	<!-- /wp:dev-talks/structured-question-group -->
+
+	<!-- wp:dev-talks/structured-question-group -->
+	<div itemtype="https://schema.org/Question" itemscope itemprop="mainEntity"><!-- wp:heading {"placeholder":"Question","hasItemProp":true} -->
+	<h2 class="wp-block-heading" itemprop="name">What tools do you use to keep track of project requirements?</h2>
+	<!-- /wp:heading -->
+
+	<!-- wp:dev-talks/structured-answer -->
+	<div itemtype="https://schema.org/Answer" itemscope itemprop="acceptedAnswer"><div itemprop="text"><!-- wp:paragraph {"placeholder":"Answer"} -->
+	<p>Hiring managers ask this to determine how you stay organized at work and how strong your time management skills are. When you answer, tell them the specific software or tools you use to stay on top of your projects.</p>
+	<!-- /wp:paragraph --></div></div>
+	<!-- /wp:dev-talks/structured-answer --></div>
+	<!-- /wp:dev-talks/structured-question-group -->
+
+	<!-- wp:dev-talks/structured-question-group -->
+	<div itemtype="https://schema.org/Question" itemscope itemprop="mainEntity"><!-- wp:heading {"placeholder":"Question","hasItemProp":true} -->
+	<h2 class="wp-block-heading" itemprop="name">What programming languages are you familiar with?</h2>
+	<!-- /wp:heading -->
+
+	<!-- wp:dev-talks/structured-answer -->
+	<div itemtype="https://schema.org/Answer" itemscope itemprop="acceptedAnswer"><div itemprop="text"><!-- wp:paragraph {"placeholder":"Answer"} -->
+	<p>When hiring managers ask this, they\'re trying to determine if your skills align with the job\'s qualifications. While you need to let them know the programming languages you have experience with, it\'s also important to show you\'re willing to learn additional languages to fit the needs of the job.</p>
+	<!-- /wp:paragraph --></div></div>
+	<!-- /wp:dev-talks/structured-answer --></div>
+	<!-- /wp:dev-talks/structured-question-group -->
+
+	<!-- wp:dev-talks/structured-question-group -->
+	<div itemtype="https://schema.org/Question" itemscope itemprop="mainEntity"><!-- wp:heading {"placeholder":"Question","hasItemProp":true} -->
+	<h2 class="wp-block-heading" itemprop="name">Describe your ideal environment as a developer</h2>
+	<!-- /wp:heading -->
+
+	<!-- wp:dev-talks/structured-answer -->
+	<div itemtype="https://schema.org/Answer" itemscope itemprop="acceptedAnswer"><div itemprop="text"><!-- wp:paragraph {"placeholder":"Answer"} -->
+	<p>Your interviewer may ask this to determine what environment is conducive to your best work as a developer. Be honest and direct with your answer, while also considering the layout and culture of the company you\'re applying to.</p>
+	<!-- /wp:paragraph --></div></div>
+	<!-- /wp:dev-talks/structured-answer --></div>
+	<!-- /wp:dev-talks/structured-question-group -->
+
+	<!-- wp:dev-talks/structured-question-group -->
+	<div itemtype="https://schema.org/Question" itemscope itemprop="mainEntity"><!-- wp:heading {"placeholder":"Question","hasItemProp":true} -->
+	<h2 class="wp-block-heading" itemprop="name">Describe a conflict you had with a team member and how you resolved it</h2>
+	<!-- /wp:heading -->
+
+	<!-- wp:dev-talks/structured-answer -->
+	<div itemtype="https://schema.org/Answer" itemscope itemprop="acceptedAnswer"><div itemprop="text"><!-- wp:paragraph {"placeholder":"Answer"} -->
+	<p>When hiring managers ask this, they\'re trying to assess your problem-solving skills. You may hear this question if the company plans to have you work within a large team or department. Your answer is a great opportunity to showcase your communication and problem-solving skills. Remember to stay positive when speaking about a former colleague.</p>
+	<!-- /wp:paragraph --></div></div>
+	<!-- /wp:dev-talks/structured-answer --></div>
+	<!-- /wp:dev-talks/structured-question-group --></div>
+	<!-- /wp:dev-talks/structured-faq --></div>
+	<!-- /wp:group -->',
+);
